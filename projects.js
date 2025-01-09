65d3e90353297f851a554cff769de7d574e99ec6
Zoksss <zorantodorovic80@gmail.com>
2025-01-09 19:30:06 +0100

    initial

---
+++ projects.js
@@ -0,0 +1,126 @@
+const projects = [
+    {
+        name: "FTN Formule",
+        subtitle: "[ React Project ]",
+        description: `This is conecept website for Boja d.o.o company which is leader in traffic signalisation in Serbia.
+        `,
+        img: "./assets/images/ftnformule.png",
+        github: "https://github.com/Zoksss/",
+        website: "https://boja.webflow.io/",
+        video: "https://www.youtube.com/@zorantodorovic5771",
+        color: "#7a5fdd"
+    },
+    {
+        name: "Quickshape",
+        subtitle: "[ React Project ]",
+        description: "Quickshape is simple game where you need to click displayed shape as fast as possible and to have fastest time possible. It features Singleplayer, Multiplayer, Currency, Shop and Inventory system.",
+        img: "./assets/images/fc_png.png",
+        github: "https://github.com/Zoksss/quickshape-react",
+        website: "https://zoksss.github.io/quickshape-react/",
+        video: "https://www.youtube.com/watch?v=clmq3-f6DmY",
+        color: "#f89b21"
+    },
+    {
+        name: "Domaci Metal Blog",
+        subtitle: "[ Webflow Project ]",
+        description: `Domaci Metal is a blog-style platform showcasing guitar tabs for trending TikTok covers. 
+        `,
+        img: "./images/domacimetal.png",
+        github: "#",
+        website: "https://domacimetal.webflow.io/",
+        video: "#",
+        color: "#fff"
+    },
+    {
+        name: "Team Gravity Landing",
+        subtitle: "[ Webflow Project ]",
+        description: "Landing page using Webflow for Team Gravity, an ctypto contept agency.",
+        img: "./assets/images/crypto_png.png",
+        github: "https://github.com/Zoksss/",
+        website: "https://teamgravity.webflow.io/",
+        video: "#",
+        color: "#a45fdd"
+    }
+]
+
+/*
+{
+        name: "FigmaLand Landing",
+        subtitle: "[ Webflow Project ]",
+        description: "Landing page using Webflow for FigmaLand, an imaginary SAAS Agency.",
+        img: "./images/figmaland.png",
+        github: "https://github.com/Zoksss/",
+        website: "https://figmaland.webflow.io/",
+        video: "https://www.youtube.com/@zorantodorovic5771"
+    }
+{
+        name: "Friend Cube",
+        subtitle: "[ React Project ]",
+        description: `Friend Cube is multiplayer times for solving Rubik's cubes.
+        <br>
+        Since there was no proper timer for multiple people solving, I decided to make one myself.
+        Friend Cube has all the necessary features including:
+        <br>
+        All players have times, averages, and other info from other players in the room.
+        All players have the same randomly generated scramble.
+        Customizable User Interface
+        It's responsive and works on all devices.
+        `,
+        img: "./images/friendcube2.png",
+        github: "https://github.com/Zoksss/friend-cube",
+        website: "https://friend-cube.herokuapp.com/",
+        video: "https://www.youtube.com/watch?v=GglIH4QvIZs"
+    },
+
+
+{
+        name: "Friend Cube",
+        subtitle: "[ Fullstack Project ]",
+        description: `Friend Cube is multiplayer times for solving Rubik's cubes.
+        <br>
+        Since there was no proper timer for multiple people solving, I decided to make one myself.
+        Friend Cube has all the necessary features including:
+        <br>
+        All players have times, averages, and other info from other players in the room.
+        All players have the same randomly generated scramble.
+        Customizable User Interface
+        It's responsive and works on all devices.
+        `,
+        img: "./images/friendcube2.png",
+        github: "https://github.com/Zoksss/friend-cube",
+        website: "https://friend-cube.herokuapp.com/",
+        video: "https://www.youtube.com/watch?v=GglIH4QvIZs"
+    },
+    {
+        name: "Quickshape",
+        subtitle: "[ Fullstack Project ]",
+        description: "Quickshape is simple game where you need to click displayed shape as fast as possible and to have fastest time possible. It features Singleplayer, Multiplayer, Currency, Shop and Inventory system.",
+        img: "./images/quickshape.png",
+        github: "https://github.com/Zoksss/quickshape",
+        website: "https://quickshape.onrender.com",
+        video: "https://www.youtube.com/watch?v=clmq3-f6DmY"
+    }
+
+
+    {
+        name: "Zoks Manager",
+        subtitle: "[ Frontend Project ]",
+        description: "Zoks Project manager is a simple manager for managing multiple projects. It has all neccasery features, including project dashboard, todo lists, task counter, date, delete projects, finish projects and more. It is build with React along with React.Spring for animations. SCSS is used for compact styling.",
+        img: "./images/zoksmanager.png",
+        github: "https://github.com/Zoksss/zoksprojectmanager",
+        website: "https://zoksss.github.io/zoksprojectmanager/",
+        video: "https://www.youtube.com/watch?v=WEThR75MYAg"
+    },
+
+
+    {
+    name: "FTN Formule",
+        subtitle: "[ Frontend Project ]",
+        description: "FTN Formule is simple learning quiz app. It is made for students learning math formulas, especially ones toward FTN University in Novi Sad, Serbia. It is build with React along with React.Spring for animations. SCSS is used for compact styling. ",
+        img: "./images/ftnformule.png",
+        github: "https://github.com/Zoksss/ftn-formule",
+        website: "https://zoksss.github.io/ftn-formule/",
+        video: "https://www.youtube.com/watch?v=mh8G05CHJTk"
+    }
+
+*/
---
+++ script.js
@@ -0,0 +1,288 @@
+const navOpen = document.querySelector("#navOpen");
+const hamburger = document.querySelector("#hamburger");
+const intro = document.querySelectorAll(".intro");
+
+
+const transitionFire = document.querySelectorAll(".link-fire");
+const homepage = document.querySelectorAll(".homepage");
+const skillsBtn = document.querySelector(".skills-button");
+
+
+const projectSubtitle = document.querySelector("#projectSubtitle");
+const projectTitle = document.querySelector("#projectTitle");
+const projectDesc = document.querySelector("#projectDesc");
+const projectImage = document.querySelector("#projectImage");
+const projectColor = document.querySelector("#projectColor");
+
+const emailbtn = document.querySelector("#emailbtn");
+const letsTalkBtn = document.querySelector("#letsTalkBtn");
+const workBtn = document.querySelector("#workBtn");
+const contactBtn = document.querySelector("#contactBtn");
+const homeBtn = document.querySelector("#homeBtn");
+
+const footerWorkBtn = document.querySelector("#footerWorkBtn");
+const footerResumeBtn = document.querySelector("#footerResumeBtn");
+const footerAboutBtn = document.querySelector("#footerAboutBtn");
+
+const liveWebsiteBtn = document.querySelector("#liveWebsiteBtn");
+const githubBtn = document.querySelector("#githubBtn");
+const videoDiv = document.querySelector("#videoDiv");
+
+const url = "http://127.0.0.1:5500"
+// https://todoroviczoran.com 
+// http://127.0.0.1:5500
+
+const contact = document.querySelector("#contact");
+
+let projectId = localStorage.getItem('projectId') || 0;
+
+const setProjectId = (n) => {
+    localStorage.setItem('projectId', n);
+    window.location.href = url + "/project.html"
+}
+
+let liveWebLink = "";
+let githubLink = "";
+let videoLink = "";
+
+hamburger.addEventListener("click", () => {
+    if (navOpen.style.display == "none") {
+        navOpen.style.display = "flex";
+        navOpen.classList.add("nav-open-anim");
+    }
+    else {
+        navOpen.classList.remove("nav-open-anim");
+        navOpen.classList.add("nav-close-anim");
+        setTimeout(() => {
+            navOpen.style.display = "none";
+            navOpen.classList.remove("nav-close-anim");
+        }, 800);
+    }
+});
+
+const setProject = () => {
+    projectId = localStorage.getItem('projectId') || 0;
+    let currProjectObj = projects[projectId];
+    console.log(projects);
+    console.log(currProjectObj);
+    projectDesc.innerHTML = currProjectObj.description;
+    projectImage.src = currProjectObj.img;
+    projectSubtitle.innerHTML = currProjectObj.subtitle;
+    projectTitle.innerHTML = currProjectObj.name;
+    projectColor.style.background = currProjectObj.color;
+    /*liveWebLink = currProjectObj.website;
+    githubLink = currProjectObj.github;
+    videoLink = currProjectObj.video; */
+}
+
+/*
+
+liveWebsiteBtn && liveWebsiteBtn.addEventListener("click", () => {
+    window.open(
+        liveWebLink,
+        "_blank"
+    )
+})
+
+githubBtn && githubBtn.addEventListener("click", () => {
+    window.open(
+        githubLink,
+        "_blank"
+    )
+})
+
+videoDiv && videoDiv.addEventListener("click", () => {
+    window.open(
+        videoLink,
+        "_blank"
+    )
+})
+
+
+footerWorkBtn && footerWorkBtn.addEventListener("click", () => {
+    window.scrollTo({
+        top: 700,
+        left: 0,
+        behavior: 'smooth'
+    });
+});
+
+letsTalkBtn && letsTalkBtn.addEventListener("click", () => {
+    window.scroll({
+        top: contact.getBoundingClientRect().top - 200,
+        left: 0,
+        behavior: 'smooth'
+    });
+})
+
+
+
+workBtn && workBtn.addEventListener("click", () => {
+    if (document.URL.includes("index.html")) {
+        navOpen.classList.remove("nav-open-anim");
+        navOpen.classList.add("nav-close-anim");
+        setTimeout(() => {
+            navOpen.style.display = "none";
+            navOpen.classList.remove("nav-close-anim");
+
+            window.scroll({
+                top: document.querySelector(".work-section").getBoundingClientRect().top - 1000,
+                left: 0,
+                behavior: 'smooth'
+            });
+        }, 500);
+    } else {
+        window.localStorage.setItem("scrollto", "work");
+        navOpen.classList.remove("nav-open-anim");
+        navOpen.classList.add("nav-close-anim");
+        console.log("test");
+        homepage[0].click();
+    }
+})
+
+
+
+homeBtn && homeBtn.addEventListener("click", () => {
+    navOpen.classList.remove("nav-open-anim");
+    navOpen.classList.add("nav-close-anim");
+});
+
+
+
+/*
+transitionFire.forEach(transitionEelemnt => {
+    transitionEelemnt.addEventListener("click", () => {
+        intro.forEach((introEl) => {
+            introEl.classList.add('intro-transition');
+            introEl.children[0].classList.add("intro-zt-transition");
+        })
+        setTimeout(() => {
+            window.location.href = url + "/project.html"
+            intro.forEach((introEl) => {
+                introEl.classList.remove("intro-transition");
+                introEl.children[0].classList.remove("intro-zt-transition");
+            });
+        }, 950)
+    })
+})
+
+
+/*
+
+contactBtn && contactBtn.addEventListener("click", () => {
+    if (document.URL.includes("index.html")) {
+        navOpen.classList.remove("nav-open-anim");
+        navOpen.classList.add("nav-close-anim");
+        setTimeout(() => {
+            navOpen.style.display = "none";
+            navOpen.classList.remove("nav-close-anim");
+
+            window.scroll({
+                top: document.querySelector("#contact").getBoundingClientRect().top - 200,
+                left: 0,
+                behavior: 'smooth'
+            });
+        }, 500);
+    } else {
+        window.localStorage.setItem("scrollto", "contact");
+        navOpen.classList.remove("nav-open-anim");
+        navOpen.classList.add("nav-close-anim");
+        console.log("test");
+        homepage[0].click();
+    }
+})
+
+homepage.forEach((homeBtn) => {
+    homeBtn.addEventListener("click", () => {
+        intro.forEach((introEl) => {
+            introEl.classList.add('intro-transition');
+            introEl.children[0].classList.add("intro-zt-transition");
+        })
+        setTimeout(() => {
+            window.location.href = url
+            intro.forEach((introEl) => {
+                introEl.classList.remove("intro-transition");
+                introEl.children[0].classList.remove("intro-zt-transition");
+            });
+        }, 950)
+    })
+});
+
+/*
+
+skillsBtn && skillsBtn.addEventListener("click", () => {
+    intro.forEach((introEl) => {
+        introEl.classList.add('intro-transition');
+        introEl.children[0].classList.add("intro-zt-transition");
+    })
+    setTimeout(() => {
+        window.location.href = url + "/skills.html"
+        intro.forEach((introEl) => {
+            introEl.classList.remove("intro-transition");
+            introEl.children[0].classList.remove("intro-zt-transition");
+        });
+    }, 950)
+})
+
+/*
+
+const setProject = () => {
+    let currProjectObj = projects[projectId];
+    console.log(projects);
+    console.log(currProjectObj);
+    projectDesc.innerHTML = currProjectObj.description;
+    projectImage.src = currProjectObj.img;
+    projectSubtitle.innerHTML = currProjectObj.subtitle;
+    projectTitle.innerHTML = currProjectObj.name;
+    liveWebLink = currProjectObj.website;
+    githubLink = currProjectObj.github;
+    videoLink = currProjectObj.video;
+}
+
+/*
+document.addEventListener('mousemove', function (e) {
+    const circle = document.querySelector(".header-profile-image");
+    //const circle2 = document.querySelector(".work-work-image");
+
+    const cursor = document.querySelector(".cursor-outer");
+    circle.style.left = e.clientX + 20 + "px";
+    circle.style.top = e.clientY + 50 + "px";
+
+    
+    let h = document.documentElement, 
+    b = document.body,
+    st = 'scrollTop',
+    sh = 'scrollHeight';
+
+    let percent = (h[st]||b[st]) / ((h[sh]||b[sh]) - h.clientHeight) * 100;
+
+
+   // circle2.style.left = e.clientX + 100 + "px";
+    //circle2.style.top = e.clientY + percent*16 + "px";
+
+    cursor.style.left = e.clientX - 20 + "px";
+    cursor.style.top = e.clientY - 20 + "px";
+
+
+    cursor.style.mixBlendMode = "difference"
+    if(document.elementFromPoint(e.clientX, e.clientY) && document.elementFromPoint(e.clientX, e.clientY).innerHTML === "Zoran Todorović,"){
+        circle.style.display = "flex";
+        cursor.style.backgroundColor = "#fff"
+        cursor.style.transform = "scale(1.4)"
+    }
+    else if(document.elementFromPoint(e.clientX, e.clientY) && document.elementFromPoint(e.clientX, e.clientY).innerHTML === "Creations"){
+       // circle2.style.display = "flex";
+        cursor.style.backgroundColor = "#fff"
+        cursor.style.transform = "scale(1.4)"
+    }
+
+    else{
+        circle.style.display = "none";
+       // circle2.style.display = "none";
+        cursor.style.backgroundColor = "transparent"
+        cursor.style.transform = "scale(1)"
+
+    }
+});
+
+*/
